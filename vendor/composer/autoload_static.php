868cfc69db023a2b785d7a589ec0f8d310627dc4
Chris Brody <chris@cbrody.com>
2018-09-25 06:59:53 +0100

    Doctrine conflict

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -415,9 +415,6 @@ class ComposerStaticInitdc417df6cd2696403bcacb909f58b9ba
         'Doctrine\\Common\\' => 
         array (
             0 => __DIR__ . '/..' . '/doctrine/common/lib/Doctrine/Common',
-            1 => __DIR__ . '/..' . '/doctrine/event-manager/lib/Doctrine/Common',
-            2 => __DIR__ . '/..' . '/doctrine/persistence/lib/Doctrine/Common',
-            3 => __DIR__ . '/..' . '/doctrine/reflection/lib/Doctrine/Common',
         ),
         'Consolidation\\OutputFormatters\\' => 
         array (
