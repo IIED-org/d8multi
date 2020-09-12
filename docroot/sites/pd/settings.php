083bfc0888b5d401f2839fff2754c081f360fe91
Chris Brody <chris@cbrody.com>
2020-09-12 11:04:59 +0100

    Update settings.php

--- docroot/sites/pd/settings.php
+++ docroot/sites/pd/settings.php
@@ -800,3 +800,7 @@ if (isset($_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR']) && file_exists($_SERVER['D
   require $_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR'] . '/cld_devcloud_landcam_dev_pd.inc';
 }
 // </DDSETTINGS>
+
+if (file_exists('/var/www/site-php')) {
+  require '/var/www/site-php/landcam/pd-settings.inc';
+}
