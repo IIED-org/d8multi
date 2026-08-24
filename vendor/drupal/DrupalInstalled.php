841cd755079a9590ff72b759dd15a37645277516
DavidIIED <davidsankar@iied.org>
2026-08-24 15:24:53 +0100

    add backup and migrate module

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '5b92d1d95af96986';
+  public const string VERSIONS_HASH = '63af0e8442013d12';
 
 }
